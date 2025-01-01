7d3541fa78e2f61627863cfb6f620aa79f04c24f
Arush <arushyadlapati.lwsd@gmail.com>
2025-01-01 12:01:00 -0800

    Rename directories

---
+++ examples/src/main/java/com/example/solverslibexamples/SharedOdometry/PositionTracker.java
@@ -0,0 +1,9 @@
+package com.example.solverslibexamples.SharedOdometry;
+
+import com.seattlesolvers.solverslib.geometry.Pose2d;
+
+public class PositionTracker {
+
+    public static Pose2d robotPose;
+
+}
