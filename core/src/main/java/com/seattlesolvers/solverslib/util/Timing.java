ba52a7b8db46daa7717bf9e84531489306d85ed0
daniel.yang <bluestonenerd@gmail.com>
2025-11-07 19:24:32 -0800

    feat + refactor: extracted Timer into Stopwatch in case you don't need a timeout. Added deltaTime (e.g. checking loop times).

--- core/src/main/java/com/seattlesolvers/solverslib/util/Timing.java
+++ core/src/main/java/com/seattlesolvers/solverslib/util/Timing.java
@@ -5,52 +5,51 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Class for a time related items.
+ * Class for a stopwatch.
  */
 public class Timing {
 
     /**
-     * Class for a timer. This can be used for checking if a duration has passed, only continuing
-     * if the timer has finished, and so forth.
+     * Class for a stopwatch. This can be used for getting the elapsed time of a process, checking
+     * the loop time, and so forth.
      * <p>
      * A more simple version of a timer better suited for quick uses rather than an
      * {@link ElapsedTime} object.
      */
-    public static class Timer {
+    public static class Stopwatch {
         private ElapsedTime time;
-        private long timerLength;
         private long pauseTime; // in nanoseconds, regardless of unit
-        private TimeUnit unit;
+        private long previousTime;
+        private final TimeUnit unit;
         private boolean timerOn;
 
         /**
          * Creates a new timer object.
          *
-         * @param timerLength The length of the timer, in the units specified by unit.
          * @param unit        The unit of timerLength.
          */
-        public Timer(long timerLength, TimeUnit unit) {
-            this.timerLength = timerLength;
+        public Stopwatch(TimeUnit unit) {
             this.unit = unit;
             this.time = new ElapsedTime();
+            this.timerOn = false;
+            this.previousTime = 0;
             time.reset();
         }
 
         /**
-         * Creates a new timer object.
-         *
-         * @param timerLength The length of the timer, in seconds.
+         * Creates a new timer object in seconds.
          */
-        public Timer(long timerLength) {
-            this(timerLength, TimeUnit.SECONDS);
+        public Stopwatch() {
+            this(TimeUnit.SECONDS);
         }
 
         /**
-         * Starts this timer.
+         * Starts or restarts this timer.
          */
         public void start() {
             time.reset();
             pauseTime = 0;
+            previousTime = 0;
             timerOn = true;
         }
 
@@ -87,6 +86,50 @@ public class Timing {
             else return unit.convert(pauseTime, TimeUnit.NANOSECONDS);
         }
 
+        public long deltaTime() {
+            return elapsedTime() - previousTime;
+        }
+
+        /**
+         * Check if this timer is running.
+         *
+         * @return True if this timer has been started and is not paused, false otherwise.
+         */
+        public boolean isTimerOn() {
+            return timerOn;
+        }
+    }
+
+    /**
+     * Class for a timer. This can be used for checking if a duration has passed, only continuing
+     * if the timer has finished, and so forth.
+     * <p>
+     * A more simple version of a timer better suited for quick uses rather than an
+     * {@link ElapsedTime} object.
+     */
+    public static class Timer extends Stopwatch {
+        private final long timerLength;
+
+        /**
+         * Creates a new timer object.
+         *
+         * @param timerLength The length of the timer, in the units specified by unit.
+         * @param unit        The unit of timerLength.
+         */
+        public Timer(long timerLength, TimeUnit unit) {
+            super(unit);
+            this.timerLength = timerLength;
+        }
+
+        /**
+         * Creates a new timer object.
+         *
+         * @param timerLength The length of the timer, in seconds.
+         */
+        public Timer(long timerLength) {
+            this(timerLength, TimeUnit.SECONDS);
+        }
+
         /**
          * Get the remaining time until this timer is done.
          *
@@ -105,15 +148,6 @@ public class Timing {
          */
         public boolean done() {
             return elapsedTime() >= timerLength;
-        }
-
-        /**
-         * Check if this timer is running.
-         *
-         * @return True if this timer has been started and is not paused, false otherwise.
-         */
-        public boolean isTimerOn() {
-            return timerOn;
         }
     }
 
